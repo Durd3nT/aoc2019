6159e3c61e90b55fc6e859c5fb3214357b540cf5
Alexander Miessen <amiessen@student.ethz.ch>
2020-12-11 17:07:04 +0100

    separated part 1 and 2

--- main01.cpp
+++ main01.cpp
@@ -9,7 +9,7 @@ template <typename T>
 void computeExtraFuel(T & massTot) {
     double mass(1);
     T massDiff(massTot);
-    std::cout << massDiff << "\n";
+    // std::cout << massDiff << "\n";
 
     while(mass > 0) {
         mass = double(massDiff)/3;
@@ -22,14 +22,19 @@ void computeExtraFuel(T & massTot) {
 }
 
 
-void computeFuel(int & massTot, std::vector<int> & modules) {
+int computeFuel(std::vector<int> & modules, bool include_fuel) {
+    int massTot = 0;
     std::vector<int>::iterator it = modules.begin();
+
     for (; it != modules.end(); ++it) {
         double mass = double(*it)/3;
         mass = std::floor(mass)-2;
-        computeExtraFuel(mass);
+        if (include_fuel) {
+            computeExtraFuel(mass);
+        }
         massTot += int(mass);
     }
+    return massTot;
 }
 
 
@@ -44,9 +49,17 @@ int main() {
         modules.assign(start, end);
     } else { std::cout << "ERROR: could not open file\n"; }
 
-    int massTot(0);
+    bool include_fuel = false;
+    int massTot = 0;
+    massTot = computeFuel(modules, include_fuel);
 
-    computeFuel(massTot, modules);
+    std::cout << "\n - - - PART 1 - - - \n";
+    std::cout << "Fuel requirements (without including fuel to carry fuel): ";
+    std::cout << massTot << "\n";
 
-    std::cout << "Total mass including fuel for fuel = " << massTot << "\n";
+    include_fuel = true;
+    massTot = computeFuel(modules, include_fuel);
+
+    std::cout << "\n - - - PART 2 - - - \n";
+    std::cout << "Total fuel requirements: " << massTot << "\n";
 }
