bd8749b036051fe5a1bdb4d02e157d9a36549bf6
Alexander Miessen <amiessen@student.ethz.ch>
2020-12-15 09:57:14 +0100

    fixed bug in day 3, entirely new data structure

--- main03.cpp
+++ main03.cpp
@@ -4,229 +4,316 @@
 #include <sstream>
 #include <vector>
 #include <limits>
+#include <cmath>
+#include <cassert>
 
 
 
-// ##########################################################################
+enum direction {up, down, left, right};
+
+std::ostream & operator<<(std::ostream & out, const direction & dir) {
+    switch (dir) {
+        case up:
+            out << "up";
+            break;
+        case down:
+            out << "down";
+            break;
+        case left:
+            out << "left";
+            break;
+        case right:
+            out << "right";
+            break;
+        default:
+            out << "No fit";
+    }
+    return out;
+}
+
+
+struct segment {
+    direction dir;
+    int dist;
+};
+
+struct wire {
+    /* edge_coords stores coordinates (x,y) of all edges of a wire, i.e., of
+       points where the wire changes direction.
+       cum_len stores the length of a wire up to each coordinate
+       edge_coords[i]
+    */
+
+    std::vector<std::vector<int>> edge_coords;
+    std::vector<long unsigned> cum_len;
+};
+
+struct intersection {
+    /* Stores:
+       - coords: (x,y) of intersction
+       - man_dist: manhatten distance to origin
+       - len_to_inter: length of all wires up to intersection
+    */
+
+    std::vector<int> coords;
+    unsigned man_dist;
+    std::vector<unsigned> len_to_inter;
+};
+
 
 // --- FUNCTIONS ---
-
-void loadData(const std::string & data_path, std::vector<char> & direction,
-              std::vector<int> & distance, std::vector<int> & lineLength)
+template <typename S>
+void loadData(const std::string & data_path,
+              std::vector<std::vector<S>> & wire_segs)
 {
-    // load CSVs of format <char><int>, e.g., R999. Store char (R) in
-    // std::vector direction, and integer (999) in std::vector distance
+    /*
+    Load CSVs from format <char><int>, e.g., R719, into a vector of objects of
+    type segment, storing the first char (direction, e.g., R) in segment.dir
+    and the rest of the string (distance, e.g., 719) in segment.dist
+    */
 
+    assert(wire_segs.empty());
     std::ifstream infile(data_path);
 
-    int lineCount = 0;
     std::string line;
     std::string walk;
+    int wire_count = 0;
 
-    while (getline(infile, line, '\n')) {
-        lineLength.push_back(0);
-        std::istringstream lineStream(line); // convert string line to istream
+    if (infile.is_open()) {
+        while (getline(infile, line, '\n')) {
+            wire_segs.resize(wire_count + 1);
 
-        while (getline(lineStream, walk, ',')) {
-            lineLength[lineCount]++;
-            direction.push_back(walk[0]); // string position 0 is direction
-            std::string dir = walk.substr(1); // from position 1 to end
-            distance.push_back(std::stoi(dir));
+            std::istringstream lineStream(line);
+            while (getline(lineStream, walk, ',')) {
+                S seg;
+
+                switch (walk[0]) {
+                    case 'U':
+                        seg.dir = up;
+                        break;
+                    case 'D':
+                        seg.dir = down;
+                        break;
+                    case 'L':
+                        seg.dir = left;
+                        break;
+                    case 'R':
+                        seg.dir = right;
+                        break;
+                    default:
+                        std::cout << "ERROR: direction letter " << walk[0];
+                        std::cout << " not supported!\n";
+                }
+
+                std::string dist = walk.substr(1);
+                seg.dist = std::stoi(dist);
+                wire_segs[wire_count].push_back(seg);
+            }
+            wire_count++;
         }
-        lineCount++;
+    } else {
+        std::cout << "Error: could not open file " << data_path << "\n";
     }
 }
 
 
-void getGridCoord(const std::vector<char> & dir, const std::vector<int> & dist,
-                  const int & edges, std::vector<int> & coord,
-                  std::vector<int> & cableLen)
+template <typename S, typename W>
+void getGridCoord(const std::vector<std::vector<S>> & wire_segs,
+                  std::vector<W> & wires)
 {
-    // for each cable, store grid coordinates of each edge (where a cable
-    // changes its direction) in std::vector coord.
-    // coord is layed out as follows:
-    // coord = (coord_cable1 , coord_cable2)
-    // with coord_cablei = (x1, y1, x2, ...)
+    /*
+    Store the wires in vector of object of type wire.
+    Each object of type wire contains a coordinate vector of all edges (i.e.,
+    changes in direction) of the wire in the coordinate (x0,y0), (x1, y1), ...
+    Each object of type wire further contains a vector containing its
+    cumulative length up to the respectuve coordinate.
+    Coordinates are obtained by iterating through the directions and distances
+    of each wire's segments.
+    */
 
-    for (int j = 0; j < 2; j++) { // loop over cables
-        int X = 0;
-        int Y = 0;
-        int L = 0;
+    assert(wires.empty() && "wires vector must be empty");
 
-        for (int i = 0; i < edges; i++) { // loop over cable edges
-            if (dir[i + j*edges] == 'R')
-                X += dist[i + j*edges];
-            else if (dir[i + j*edges] == 'L')
-                X -= dist[i + j*edges];
-            else if (dir[i + j*edges] == 'U')
-                Y += dist[i + j*edges];
-            else if (dir[i + j*edges] == 'D')
-                Y -= dist[i + j*edges];
+    wires.resize(wire_segs.size());
 
-            coord[2*i + 2*j*edges] = X;
-            coord[2*i+1 + 2*j*edges] = Y;
+    for (size_t i = 0; i < wire_segs.size(); i++) { // loop over wires
+        int x = 0;
+        int y = 0;
+        int l = 0;
 
-            L += dist[i + j*edges];
-            cableLen[i + j*edges] = L;
-        } // for i < edgeCount
-    } // for j < number of cables
-}
+        wires[i].edge_coords.push_back(std::vector<int>{x, y});
+        wires[i].cum_len.push_back(l);
 
+        for (size_t j = 0; j < wire_segs[i].size(); j++) {
+            switch (wire_segs[i][j].dir) {
+                case up:
+                    y += wire_segs[i][j].dist;
+                    break;
+                case down:
+                    y -= wire_segs[i][j].dist;
+                    break;
+                case left:
+                    x -= wire_segs[i][j].dist;
+                    break;
+                case right:
+                    x += wire_segs[i][j].dist;
+                    break;
+            }
+            l += wire_segs[i][j].dist;
 
-bool vertical(const int & x1, const int & x2)
-{
-    // is line horizontal between x1 and x2 horizontal?
-    return x1 == x2;
-}
-
-
-bool parallel(std::vector<int>::const_iterator start1,
-              std::vector<int>::const_iterator start2)
-{
-    // are lines with x-coordinates *start1, etc., respectively, parallel?
-    return (vertical(*start1, *(start1+2)) == vertical(*start2, *(start2+2)));
+            wires[i].edge_coords.push_back(std::vector<int>{x, y});
+            wires[i].cum_len.push_back(l);
+        }
+    }
 }
 
 
 bool inbetween(const int & x, const int & a, const int & b) {
-    // is x in [a,b] or [b,a]?
+    /* Is x in [a,b] or [b,a]?
+    */
+
     return ( (x > a && x < b) || (x < a && x > b) );
 }
 
 
-bool intersect(std::vector<int>::const_iterator start1,
-               std::vector<int>::const_iterator start2)
+bool intersect(std::vector<std::vector<int>>::const_iterator coord1,
+               std::vector<std::vector<int>>::const_iterator coord2)
 {
-    // do the two lines starting at *start1 and *start2, respectively,
-    // intersect?
-    if ( inbetween(*start2, *start1, *(start1+2))
-         && inbetween(*(start1+1), *(start2+1), *(start2+3)) ) {
+    /* Do the two lines starting at *coord1 and *coord2, respectively,
+       intersect?
+    */
+
+    if ( inbetween((*coord2)[0], (*coord1)[0], (*(coord1 + 1))[0])
+         && inbetween((*coord1)[1], (*coord2)[1], (*(coord2 + 1))[1]) ) {
         return true;
-    } else if ( inbetween(*start1, *start2, *(start2+2))
-         && inbetween(*(start2+1), *(start1+1), *(start1+3)) ) {
+    } else if ( inbetween((*coord1)[0], (*coord2)[0], (*(coord2 + 1))[0])
+         && inbetween((*coord2)[1], (*coord1)[1], (*(coord1 + 1))[1]) ) {
         return true;
     } else { return false; }
 }
 
 
-void computeIntersection(std::vector<int>::const_iterator start1,
-                         std::vector<int>::const_iterator start2,
+void computeIntersection(std::vector<std::vector<int>>::const_iterator coord1,
+                         std::vector<std::vector<int>>::const_iterator coord2,
                          int & x, int & y)
 {
-    // - compute intersection between lines starting at *start1 and *start2,
-    // respectively, store coordinates of intersection in x and y
-    if (vertical(*start1, *(start1+2))) {
-        x = (*start1);
-        y = (*(start2+1));
+    /* Compute intersection between lines starting at *coord1 and *coord2,
+       respectively, store coordinates of intersection in x and y.
+    */
+
+    if ((*coord1)[0] == (*(coord1 + 1))[0]) {
+        x = (*coord1)[0];
+        y = (*coord2)[1];
     } else {
-        x = (*start2);
-        y = (*(start1+1));
+        x = (*coord2)[0];
+        y = (*coord1)[1];
     }
 }
 
 
-int getSigDelay(std::vector<int>::const_iterator start1,
-                std::vector<int>::const_iterator start2,
-                const int & cableLen1, const int & cableLen2,
-                int & x, int & y)
+template <typename W, typename I>
+void get_all_intersections(const std::vector<W> & wires,
+                           std::vector<I> & inters)
 {
-    // return combined steps both cables take to reach an intersection
-    int len1 = cableLen1 + std::abs(*start1 - x) + std::abs(*(start1+1) - y);
-    int len2 = cableLen2 + std::abs(*start2 - x) + std::abs(*(start2+1) - y);
+    /*
+    Finds the coordinates of all intersections by looping through the
+    edge-coordinates of both cables and checking whether they cross.
+    Confirmed intersections are stored in a vector of objects of type
+    intersection, with coordinates of the intersection in intersection.coords,
+    as well as manhatten distance and cumulative length up to that
+    intersection.
+    */
 
-    return (len1 + len2);
+    assert(inters.empty() && "inters vector must be empty");
+
+    for (size_t i = 0; i < wires[0].edge_coords.size()-1; i++) {
+        for (size_t j = 0; j < wires[1].edge_coords.size()-1; j++) {
+            // compute intersection
+            std::vector<std::vector<int>>::const_iterator it0 = wires[0].edge_coords.begin();
+            std::vector<std::vector<int>>::const_iterator it1 = wires[1].edge_coords.begin();
+
+            if (intersect(it0 + i, it1 + j)) {
+                int x = 0;
+                int y = 0;
+                computeIntersection(it0 + i, it1 + j, x, y);
+
+                I inter;
+                inter.coords = std::vector<int>{x, y};
+                inter.man_dist = std::abs(x) + std::abs(y);
+                inter.len_to_inter.push_back(wires[0].cum_len[i]
+                                             + std::abs((*(it0 + i))[0] - x)
+                                             + std::abs((*(it0 + i))[1] - y));
+                inter.len_to_inter.push_back(wires[1].cum_len[j]
+                                             + std::abs((*(it1 + j))[0] - x)
+                                             + std::abs((*(it1 + j))[1] - y));
+
+                inters.push_back(inter);
+            }
+        }
+    }
 }
 
 
-void getAllIntersections(const std::vector<int> & coord,
-                         const std::vector<int> & cableLen,
-                         const int & edges, std::vector<int> & inter,
-                         int & minComLen)
-{
-    // - For both cables, loop through coordinates of all their edges (where
-    // the cable changes direction) and compare all line elements of one cable
-    // (straight sections of cable) to all line elements of other cable to
-    // to check for intersections of the two cables.
-    // - Store x- and y-grid-coordinates of each intersection in std::vector inter
-    // - Additionally, get minimum combined length of both cables to reach an
-    // intersection.
+template <typename I>
+unsigned find_closest_intersection(const std::vector<I> & inters) {
+    /* Find intersection with minimum manhatten distance.
+    */
 
-    for (int i = 0; i < edges-1; i++) { // loop over cable edges
-        for (int j = 0; j < edges-1; j++) { // loop over cable edges
+    unsigned min = std::numeric_limits<int>::max();
 
-            if ( !parallel(coord.begin()+2*i, coord.begin()+2*j+2*edges) ) {
-                bool cablesCross = intersect(coord.begin()+2*i,
-                                             coord.begin()+2*j+2*edges);
-                // compute intersection
-                if (cablesCross) {
-                    int x = 0;
-                    int y = 0;
-                    computeIntersection(coord.begin()+2*i,
-                                        coord.begin()+2*j+2*edges,
-                                        x, y);
-                    inter.push_back(x);
-                    inter.push_back(y);
-
-                    // compute combined cable length to intersection
-                    int combinedLen = 0;
-                    combinedLen = getSigDelay(coord.begin()+2*i,
-                                        coord.begin()+2*j+2*edges,
-                                        cableLen[i], cableLen[j+edges],
-                                        x, y);
-                    if (combinedLen < minComLen || minComLen == 0) {
-                        minComLen = combinedLen;
-                    }
-                } // IF: cablesCross
-            } // IF: cables are NOT parallel
-        } // FOR j < edges-1
-    } // FOR i < edges-1
-}
-
-
-int findClosestIntersection(const std::vector<int> & inter) {
-    size_t n = inter.size()/2;
-    int min = std::numeric_limits<int>::max();
-
-    for (size_t i = 0; i < n; i++) {
-        int dist = inter[2*i] + inter[2*i+1];
-        if (dist < min) { min = dist; }
+    for (size_t i = 0; i < inters.size(); i++) {
+        if (inters[i].man_dist < min && inters[i].man_dist != 0) {
+            min = inters[i].man_dist;
+        }
     }
 
     return min;
 }
 
-// ##########################################################################
+
+template <typename I>
+unsigned find_min_comb_len(const std::vector<I> & inters) {
+    /* Find intersection with minimum combined cable length up to the
+       intersection.
+    */
+
+    unsigned min = std::numeric_limits<int>::max();
+
+    for (size_t i = 0; i < inters.size(); i++) {
+        unsigned com_len = (inters[i].len_to_inter[0] + inters[i].len_to_inter[1]);
+        if (com_len < min && inters[i].man_dist != 0) {
+            min = com_len;
+        }
+    }
+
+    return min;
+}
 
 
-// ################
+// - - - - - - - - -
 // ----- MAIN -----
-// ################
+// - - - - - - - - -
 int main() {
     std::string data_path = "./input_files/in03.txt";
 
-    std::vector<char> direction;
-    std::vector<int> distance, edgeCount;
+    std::vector<std::vector<segment>> wire_segs;
+    loadData<segment>(data_path, wire_segs);
 
-    loadData(data_path, direction, distance, edgeCount);
+    std::vector<wire> wires;
+    getGridCoord<segment, wire>(wire_segs, wires);
 
-    if (edgeCount[0] == edgeCount[1]) {
-        std::vector<int> gridCoord;
-        std::vector<int> cableLen;
-        // store x,y coords next to each other, as they will be accessed
-        // together and for both cables in the same vector
-        gridCoord.resize(2*direction.size()); // (x1, y1, x2, y2, ...)
-        cableLen.resize(direction.size()); // (len at (x1,y1), at (x2,y2), ...)
-        getGridCoord(direction, distance, edgeCount[0], gridCoord, cableLen);
+    std::vector<intersection> inters;
+    get_all_intersections(wires, inters);
 
-        std::vector<int> inter;
-        int minComLen = 0;
-        getAllIntersections(gridCoord, cableLen, edgeCount[0],
-                            inter, minComLen);
-        int closestIntersection = findClosestIntersection(inter);
+    unsigned closest = find_closest_intersection(inters);
 
-        std::cout << "Closest intersection is ";
-        std::cout << closestIntersection << " away\n";
-        std::cout << "Minimum combined cable length to intersection is ";
-        std::cout << minComLen << "\n";
-    } else { std::cout << "ERROR: lines have different length\n"; }
+    std::cout << "\n - - - PART 1 - - - \n";
+    std::cout << "Closest intersection is ";
+    std::cout << closest << " away\n";
+
+    unsigned min_com_len = find_min_comb_len(inters);
+
+    std::cout << "\n - - - PART 2 - - - \n";
+    std::cout << "Minimum combined cable length to intersection is ";
+    std::cout << min_com_len << "\n";
+
 }
